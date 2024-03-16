e48f8270d31880da8789b372afcee7e240358a3a
Christian <christian@qode.io>
2024-03-16 09:30:14 +0100

    main: Add Feature Flag support

--- src/LaravelPosthog.php
+++ src/LaravelPosthog.php
@@ -4,9 +4,12 @@ namespace QodeNL\LaravelPosthog;
 
 use Auth;
 use Log;
+use PostHog\PostHog;
 use QodeNL\LaravelPosthog\Jobs\PosthogAliasJob;
+use QodeNL\LaravelPosthog\Jobs\PosthogBaseJob;
 use QodeNL\LaravelPosthog\Jobs\PosthogCaptureJob;
 use QodeNL\LaravelPosthog\Jobs\PosthogIdentifyJob;
+use function Symfony\Component\Translation\t;
 
 class LaravelPosthog
 {
@@ -54,6 +57,63 @@ class LaravelPosthog
         } else {
             Log::debug('PosthogAliasJob not dispatched because posthog is disabled');
         }
+    }
+
+    public function isFeatureEnabled(
+        string $featureKey,
+        array $groups = [],
+        array $personProperties = [],
+        array $groupProperties = [],
+    ): bool {
+        return (bool) $this->getFeatureFlag(
+            $featureKey,
+            $groups,
+            $personProperties,
+            $groupProperties,
+        );
+    }
+
+    public function getFeatureFlag(
+        string $featureKey,
+        array $groups = [],
+        array $personProperties = [],
+        array $groupProperties = [],
+    ): null|bool|string {
+        if ($this->posthogEnabled()) {
+            (new PosthogBaseJob())->init();
+
+            return Posthog::getFeatureFlag(
+                $featureKey,
+                $this->sessionId,
+                $groups,
+                $personProperties,
+                $groupProperties,
+                config('posthog.feature_flags.evaluate_locally') ?? false,
+                config('posthog.feature_flags.send_events') ?? true,
+            );
+        }
+
+        return config('posthog.feature_flags.default_enabled') ?? false;
+    }
+
+    public function getAllFlags(
+        array $groups = [],
+        array $personProperties = [],
+        array $groupProperties = [],
+    ): array {
+        if ($this->posthogEnabled()) {
+            (new PosthogBaseJob())->init();
+
+            return Posthog::getAllFlags(
+                $this->sessionId,
+                $groups,
+                $personProperties,
+                $groupProperties,
+                config('posthog.feature_flags.evaluate_locally') ?? false
+            );
+        }
+
+        return [];
     }
 
 }
